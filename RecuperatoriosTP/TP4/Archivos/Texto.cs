2a8c697e5c43838284eba97251ca5ea28aa3136f
LauraCeciliaBohorquez <gerardobohorquez.95@gmail.com>
2017-07-21 07:55:57 -0300

    REC
    TPS

---
+++ RecuperatoriosTP/TP4/Archivos/Texto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        private string ArchivoString;
+
+        public Texto(string archivo)
+        {
+            ArchivoString = archivo;           
+        }
+
+        public bool Guardar(string datos)
+        {
+            bool respuesta= false;
+
+            using(StreamWriter sw = new StreamWriter(ArchivoString, true))
+            {
+                sw.WriteLine(datos);
+                respuesta = true;
+            }
+            return respuesta;
+        }
+
+        public bool Leer(out List<string> lista)
+        {
+            lista = new List<string>();
+            bool respuesta = false;
+            
+            using (StreamReader sr = new StreamReader(ArchivoString))
+            {
+                while (!sr.EndOfStream)
+                {
+                    lista.Add(sr.ReadLine());
+                }
+                respuesta = true;
+            }
+
+            return respuesta;
+        }
+    }
+}
---
+++ RecuperatoriosTP/TP4/Navegador/frmWebBrowser.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Threading;
+using Hilo;
+using System.Net;
+
+namespace Navegador
+{
+    public partial class frmWebBrowser : Form
+    {
+        private const string ESCRIBA_AQUI = "Escriba aquí...";
+        Archivos.Texto archivos;
+
+        public frmWebBrowser()
+        {
+            InitializeComponent();
+        }
+
+        private void frmWebBrowser_Load(object sender, EventArgs e)
+        {
+            this.txtUrl.SelectionStart = 0;  //This keeps the text
+            this.txtUrl.SelectionLength = 0; //from being highlighted
+            this.txtUrl.ForeColor = Color.Gray;
+            this.txtUrl.Text = frmWebBrowser.ESCRIBA_AQUI;
+            this.tspbProgreso.Minimum = 0;
+            this.tspbProgreso.Maximum = 100;
+
+
+            this.archivos = new Archivos.Texto(frmHistorial.ARCHIVO_HISTORIAL);
+        }
+
+        #region "Escriba aquí..."
+        private void txtUrl_MouseMove(object sender, MouseEventArgs e)
+        {
+            Cursor.Current = Cursors.IBeam; //Without this the mouse pointer shows busy
+        }
+
+        private void txtUrl_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (this.txtUrl.Text.Equals(frmWebBrowser.ESCRIBA_AQUI) == true)
+            {
+                this.txtUrl.Text = "";
+                this.txtUrl.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtUrl_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (this.txtUrl.Text.Equals(null) == true || this.txtUrl.Text.Equals("") == true)
+            {
+                this.txtUrl.Text = frmWebBrowser.ESCRIBA_AQUI;
+                this.txtUrl.ForeColor = Color.Gray;
+            }
+        }
+
+        private void txtUrl_MouseDown(object sender, MouseEventArgs e)
+        {
+            this.txtUrl.SelectAll();
+        }
+        #endregion
+
+        delegate void ProgresoDescargaCallback(int progreso);
+
+        private void ProgresoDescarga(int progreso)
+        {
+            if (statusStrip.InvokeRequired)
+            {
+                ProgresoDescargaCallback d = new ProgresoDescargaCallback(ProgresoDescarga);
+                this.Invoke(d, new object[] { progreso });
+            }
+            else
+            {
+                tspbProgreso.Value = progreso;
+            }
+        }
+        delegate void FinDescargaCallback(string html);
+        private void FinDescarga(string html)
+        {
+            if (rtxtHtmlCode.InvokeRequired)
+            {
+                FinDescargaCallback d = new FinDescargaCallback(FinDescarga);
+                this.Invoke(d, new object[] { html });
+            }
+            else
+            {
+                rtxtHtmlCode.Text = html;
+            }
+        }
+
+        private void btnIr_Click(object sender, EventArgs e)
+        {
+            String requestedURL = this.cleanUrl(this.txtUrl.Text);
+            this.txtUrl.Text = requestedURL;
+            this.downloadData(requestedURL);
+            this.archivos.Guardar(requestedURL);
+        }
+
+        private void downloadData(String url)
+        {
+            this.tspbProgreso.Value = 0;
+            Uri uri = new Uri(url);
+            Hilo.Descargador downloader = new Hilo.Descargador(uri, this.onDownloadProgressChanged, this.onDownloadCompleted);
+            downloader.IniciarDescarga();
+        }
+
+        private void onDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            this.tspbProgreso.Value = e.ProgressPercentage;
+        }
+
+        private void onDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            try
+            {
+                this.tspbProgreso.Value = 100;
+                this.rtxtHtmlCode.Text = e.Result;
+            }
+            catch(Exception ex)
+            {
+                throw ex.InnerException;
+            }
+        }
+
+        private String cleanUrl(String requestedURL)
+        {
+           return "http://" + requestedURL;
+
+        }
+
+        private void mostrarTodoElHistorialToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmHistorial historia = new frmHistorial();
+            historia.Show();
+        }
+
+    }
+}
